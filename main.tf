a0520b662b9d990c405d66eac7d7b8d2cbe3c64f
Ganesh Suryawanshi <shriganeshsuryawanshi44@gmail.com>
2026-02-24 10:46:14 +0530

    Add AWS provider configuration and VPC setup

---
+++ main.tf
@@ -0,0 +1,13 @@
+# AWS Provider Configuration
+provider "aws" {
+  region = "us-east-1"
+}
+
+# VPC Setup
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
